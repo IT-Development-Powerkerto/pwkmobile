77314d134c461a91234412c08b3196d115fb526e
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-03-17 21:13:11 +0700

    show data leads from API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {View, Image, StyleSheet, Text, TouchableOpacity, ImageBackground} from "react-native";
+import {View, Image, StyleSheet, Text, TouchableOpacity, ScrollView} from "react-native";
 import { StatusBar } from 'react-native';
 import Api from "../../Api";
 import { Profile } from "../../assets";
 import { Gap, InfoCard } from "../../components";
-import { getData } from "../../utils";
+import { colors, getData } from "../../utils";
 
 const Home = ({navigation}) => {
 const [name, setName] = useState('')
@@ -14,6 +14,15 @@ const [totalLead, setTotalLead] = useState('')
 const [totalClosing, setTotalClosing] = useState('')
 const [dailyLead, setDailyLead] = useState('')
 const [dailyClosing, setDailyClosing] = useState('')
+const [lead, setLead] = useState([
+    {
+        customer_name: '',
+        customer_whatsapp: '',
+        status: '',
+        created_at: '',
+        updated_at: ''
+    }
+])
 
 useEffect(() => {
     getData('user'). then(res=>{
@@ -24,12 +33,14 @@ useEffect(() => {
             setRole(response.data[0].role.name)
             setImage(response.data[0].image)
 
+
             setTotalLead(responseLead.data.total_lead)
             setTotalClosing(responseLead.data.total_closing)
             setDailyLead(responseLead.data.daily_lead)
             setDailyClosing(responseLead.data.daily_closing)
-            
-            // console.log(responseLead.data)
+            // console.log(responseLead.data.leads)
+            setLead(responseLead.data.leads)
+            // console.log('cust', lead)
         }
         userID()
         
@@ -38,61 +49,88 @@ useEffect(() => {
 return (
     <View style={styles.container}>
         <StatusBar barStyle = "default" hidden = {false} backgroundColor = "#009EF7" translucent = {true}/>
-        <View style={styles.Header}>
-            <View>
-                <Text style={{ color: 'white', fontFamily: 'Poppins-Bold', fontSize: 24 }}>{`Hello, ${name}`} </Text>
-                <Text style={{ color: 'white', fontFamily: 'Poppins-Regular', fontSize: 14 }}>{role}</Text>
-            </View>
-            <View>
-                <Image source={{ uri:`http://mobile.pwkbackoffice.com/${image}` }} style={{ width: 54, height: 54, borderRadius: 27, }}/>
-            </View>
-        </View>
-        <Gap height={10} />
-        <View style={styles.mainCard}>
-            <View style={styles.FirstCard}>
-                <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{totalLead}</Text>
-                <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Total Lead</Text>
-            </View>   
-            <View style={styles.SecondCard}>
-                <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{totalClosing}</Text>
-                <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Total Closing</Text>
-            </View> 
-        </View>
-        <View style={styles.mainCard}>
-            <View style={styles.ThirdCard}>
-                <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{dailyLead}</Text>
-                <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Daily Lead</Text>
-            </View>   
-            <View style={styles.FourtCard}>
-                <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{dailyClosing}</Text>
-                <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Daily Closing</Text>
-            </View> 
-        </View>
-        <View style={{ paddingHorizontal: 20, flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, alignItems: 'center' }}>
-            <View>
-                <Text style={{ fontSize: 18, fontFamily: 'Poppins-SemiBold', color: '#1F2432' }}>Daily Lead</Text>
-                <Text style={{ fontSize: 12, fontFamily: 'Poppins-SemiBold', color: '#A3A3A3' }}>{`${dailyLead} Leads`}</Text>
-            </View>
-            <TouchableOpacity>
-                <Text style={{ color: '#166ED8', fontFamily: 'Poppins-SemiBold', }}>+ Manual Lead</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.CardLead}>
-            <View style={styles.FirstCardLead}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 15}}>
-                    <Text style={{ fontSize: 14, fontFamily: 'Poppins-SemiBold', color: '#A3A3A3' }}>2022-03-01 03:23:35</Text>
-                    <Text style={{ fontSize: 14, fontFamily: 'Poppins-SemiBold', color: '#F70000' }}>02:50</Text>
+        
+            <View style={styles.Header}>
+                <View>
+                    <Text style={{ color: 'white', fontFamily: 'Poppins-Bold', fontSize: 24 }}>{`Hello, ${name}`} </Text>
+                    <Text style={{ color: 'white', fontFamily: 'Poppins-Regular', fontSize: 14 }}>{role}</Text>
                 </View>
-                <Gap height={10} />
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 15}}>
-                    <Text style={{ fontSize: 14, fontFamily: 'Poppins-Bold', color: '#000' }}>Deni Sumarna</Text>
-                    <Text style={{ fontSize: 14, fontFamily: 'Poppins-Bold', color: '#009EF7'}} >6285709114580</Text>
-                    <View style={{ width: 75, height: 22, borderRadius: 50, backgroundColor: '#F7CCCC', alignItems: 'center' }}>
-                        <Text style={{ color: '#F70000', fontFamily: 'Poppins-SemiBold' }}>Waiting</Text>
+                <View>
+                    <Image source={{ uri:`http://mobile.pwkbackoffice.com/${image}` }} style={{ width: 54, height: 54, borderRadius: 27, }}/>
+                </View>
+            </View>
+            <Gap height={10} />
+            <View style={styles.mainCard}>
+                <View style={styles.FirstCard}>
+                    <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{totalLead}</Text>
+                    <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Total Lead</Text>
+                </View>   
+                <View style={styles.SecondCard}>
+                    <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{totalClosing}</Text>
+                    <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Total Closing</Text>
+                </View> 
+            </View>
+            <View style={styles.mainCard}>
+                <View style={styles.ThirdCard}>
+                    <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{dailyLead}</Text>
+                    <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Daily Lead</Text>
+                </View>   
+                <View style={styles.FourtCard}>
+                    <Text style={{ fontSize: 28, fontFamily: 'Poppins-Bold', color: '#fff', top: 7 }}>{dailyClosing}</Text>
+            
+                    <Text style={{ fontSize: 22, fontFamily: 'Poppins-Medium', color: '#fff', bottom: 7 }}>Daily Closing</Text>
+                </View> 
+            </View>
+            <View style={{ paddingHorizontal: 20, flexDirection: 'row', justifyContent: 'space-between', marginTop: 30, alignItems: 'center' }}>
+                <View>
+                    <Text style={{ fontSize: 18, fontFamily: 'Poppins-SemiBold', color: '#1F2432' }}>Daily Lead</Text>
+                    <Text style={{ fontSize: 12, fontFamily: 'Poppins-SemiBold', color: '#A3A3A3' }}>{`${dailyLead} Leads`}</Text>
+                </View>
+                <TouchableOpacity>
+                    <Text style={{ color: '#166ED8', fontFamily: 'Poppins-SemiBold', }}>+ Manual Lead</Text>
+                </TouchableOpacity>
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+
+            {lead.map(data=>{
+                return (
+                <View style={styles.CardLead}>
+                    <View style={styles.FirstCardLead}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 15}}>
+                            <Text style={{ fontSize: 11, fontFamily: 'Poppins-SemiBold', color: '#A3A3A3' }}>{data.created_at}</Text>
+                            <Text style={{ fontSize: 11, fontFamily: 'Poppins-SemiBold', color: '#F70000' }}>{(new Date(data.created_at))}</Text>
+                        </View>
+                        <Gap height={10} />
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center', paddingHorizontal: 15}}>
+                            <Text style={{ fontSize: 11, fontFamily: 'Poppins-Bold', color: '#000', flexBasis: '40%' }}>{data.customer_name}</Text>
+                            <Text style={{ fontSize: 11, fontFamily: 'Poppins-Bold', color: '#009EF7', flexBasis: '35%'}} >{data.customer_whatsapp}</Text>
+                            {data.status == "Waiting" && 
+                            <View style={{  borderRadius: 50, backgroundColor: colors._redOp, alignItems: 'center', justifyContent: 'center', flexBasis: '25%' }}>
+                                <Text style={{ color: colors._red, fontFamily: 'Poppins-SemiBold', fontSize: 11 }}>{data.status}</Text>
+                            </View>
+                            }
+                            {data.status == "Processing" && 
+                            <View style={{  borderRadius: 50, backgroundColor: colors._blueOp, alignItems: 'center', justifyContent: 'center', flexBasis: '25%' }}>
+                                <Text style={{ color: colors._blue, fontFamily: 'Poppins-SemiBold', fontSize: 11 }}>{data.status}</Text>
+                            </View>
+                            }
+                            {data.status == "Closing" && 
+                            <View style={{  borderRadius: 50, backgroundColor: colors._greenOp, alignItems: 'center', justifyContent: 'center', flexBasis: '25%' }}>
+                                <Text style={{ color: colors._green, opacity: 1, fontFamily: 'Poppins-SemiBold', fontSize: 11 }}>{data.status}</Text>
+                            </View>
+                            }
+                            {data.status == "Spam" && 
+                            <View style={{  borderRadius: 50, backgroundColor: colors._grayOp, alignItems: 'center', justifyContent: 'center', flexBasis: '25%' }}>
+                                <Text style={{ color: colors._gray, fontFamily: 'Poppins-SemiBold', fontSize: 11 }}>{data.status}</Text>
+                            </View>
+                            }
+
+                        </View>
                     </View>
                 </View>
-            </View>
-        </View>
+                )
+            })}
+            </ScrollView>
     </View>
  );
 };
---
+++ src/utils/colors/index.js
@@ -0,0 +1,21 @@
+const mainColor = {
+    _red: '#F70000',
+    _redOp: 'rgba(247, 0, 0, 0.2)',
+    _green: '#23F700',
+    _greenOp: 'rgba(35, 247, 0, 0.2)',
+    _blue: '#009EF7',
+    _blueOp: 'rgba(0, 158, 247, 0.2)',
+    _gray: '#A3A3A3',
+    _grayOp: 'rgba(163, 163, 163, 0.2)'
+}
+
+export const colors = {
+    _red: mainColor._red,
+    _redOp: mainColor._redOp,
+    _green: mainColor._green,
+    _greenOp: mainColor._greenOp,
+    _blue: mainColor._blue,
+    _blueOp: mainColor._blueOp,
+    _gray: mainColor._gray,
+    _grayOp: mainColor._grayOp,
+}
