cbf91bbd04d6be4769f5c0653b1b923ec6231dfd
fakhriazzz <97779394+fakhriazzz@users.noreply.github.com>
2022-05-27 09:33:50 +0700

    merapikan list lead

--- src/components/atoms/ListLead/index.js
+++ src/components/atoms/ListLead/index.js
@@ -9,26 +9,26 @@ const ListLead = ({ customer_name, customer_whatsapp, status, created_at, onPres
             <Text style={{ fontSize: 10, fontFamily: colors._textGray, color: colors._textGray }}>{created_at}</Text>
             <Gap height={10} />
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center' }}>
-                <Text style={{ fontSize: 10, fontFamily: fonts.primary[600], color: colors._textBlack }}>{customer_name}</Text>
-                <Text style={{ fontSize: 10, fontFamily: fonts.primary[600], color: colors._blue3 }} >{customer_whatsapp}</Text>
+                <Text numberOfLines={1} style={{ fontSize: 10, fontFamily: fonts.primary[600], color: colors._textBlack, width: '30%' }}>{customer_name}</Text>
+                <Text numberOfLines={1} style={{ fontSize: 10, fontFamily: fonts.primary[600], color: colors._blue3, width: '30%' }} >{customer_whatsapp}</Text>
                 {status == "Waiting" &&
-                    <View style={{ borderRadius: 50, backgroundColor: colors._redOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2 }}>
-                        <Text style={{ color: colors._red, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
+                    <View style={{ borderRadius: 50, backgroundColor: colors._redOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2, width: '30%' }}>
+                        <Text numberOfLines={1} style={{ color: colors._red, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
                     </View>
                 }
                 {status == "Processing" &&
-                    <View style={{ borderRadius: 50, backgroundColor: colors._blueOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2 }}>
-                        <Text style={{ color: colors._blue, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
+                    <View style={{ borderRadius: 50, backgroundColor: colors._blueOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2, width: '30%' }}>
+                        <Text numberOfLines={1} style={{ color: colors._blue, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
                     </View>
                 }
                 {status == "Closing" &&
-                    <View style={{ borderRadius: 50, backgroundColor: colors._greenOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2 }}>
-                        <Text style={{ color: colors._green, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
+                    <View style={{ borderRadius: 50, backgroundColor: colors._greenOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2, width: '30%' }}>
+                        <Text numberOfLines={1} style={{ color: colors._green, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
                     </View>
                 }
                 {status == "Spam" &&
-                    <View style={{ borderRadius: 50, backgroundColor: colors._grayOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2 }}>
-                        <Text style={{ color: colors._gray, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
+                    <View style={{ borderRadius: 50, backgroundColor: colors._grayOp, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 8, paddingVertical: 2, width: '30%' }}>
+                        <Text numberOfLines={1} style={{ color: colors._gray, fontFamily: fonts.primary[600], fontSize: 10 }}>{status}</Text>
                     </View>
                 }
             </View>
